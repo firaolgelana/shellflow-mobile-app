e440f95562082a03b5e3d343b7c931b83ec70ffa
Firaol Gelana <gelanafiraol@gmail.com>
2026-01-04 16:28:30 +0300

    feat(social):add bloc implementation

--- lib/features/social/presentation/bloc/social_event.dart
+++ lib/features/social/presentation/bloc/social_event.dart
@@ -1,8 +1,89 @@
 part of 'social_bloc.dart';
 
-sealed class SocialEvent extends Equatable {
+abstract class SocialEvent extends Equatable {
   const SocialEvent();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
+}
+
+// --- Feed & Tasks ---
+
+class GetSocialFeedEvent extends SocialEvent {
+  final int page;
+  final int limit;
+  const GetSocialFeedEvent({this.page = 1, this.limit = 20});
+}
+
+class ShareTaskToFeedEvent extends SocialEvent {
+  final String taskId;
+  final String? caption;
+  const ShareTaskToFeedEvent({required this.taskId, this.caption});
+}
+
+class ToggleLikeTaskEvent extends SocialEvent {
+  final String taskId;
+  const ToggleLikeTaskEvent({required this.taskId});
+}
+
+class GetSharedTaskDetailsEvent extends SocialEvent {
+  final String taskId;
+  const GetSharedTaskDetailsEvent({required this.taskId});
+}
+
+// --- Comments ---
+
+class GetTaskCommentsEvent extends SocialEvent {
+  final String taskId;
+  const GetTaskCommentsEvent({required this.taskId});
+}
+
+class AddCommentEvent extends SocialEvent {
+  final String taskId;
+  final String content;
+  const AddCommentEvent({required this.taskId, required this.content});
+}
+
+class DeleteCommentEvent extends SocialEvent {
+  final String commentId;
+  const DeleteCommentEvent({required this.commentId});
+}
+
+// --- Friends & Connections ---
+
+class GetFriendListEvent extends SocialEvent {
+  final String? userId; // If null, gets current user's friends
+  const GetFriendListEvent({this.userId});
+}
+
+class SearchUsersEvent extends SocialEvent {
+  final String query;
+  const SearchUsersEvent({required this.query});
+}
+
+class SendConnectionRequestEvent extends SocialEvent {
+  final String targetUserId;
+  const SendConnectionRequestEvent({required this.targetUserId});
+}
+
+class GetPendingRequestsEvent extends SocialEvent {
+  const GetPendingRequestsEvent();
+}
+
+class HandleConnectionRequestEvent extends SocialEvent {
+  final String requestId;
+  final ConnectionRequestAction action; // Defined in repo/entity
+  const HandleConnectionRequestEvent({required this.requestId, required this.action});
+}
+
+class RemoveFriendEvent extends SocialEvent {
+  final String friendId;
+  const RemoveFriendEvent({required this.friendId});
+}
+
+// --- User Profile ---
+
+class GetSocialUserProfileEvent extends SocialEvent {
+  final String userId;
+  const GetSocialUserProfileEvent({required this.userId});
 }
--- lib/features/social/presentation/bloc/social_state.dart
+++ lib/features/social/presentation/bloc/social_state.dart
@@ -1,10 +1,96 @@
 part of 'social_bloc.dart';
 
-sealed class SocialState extends Equatable {
+abstract class SocialState extends Equatable {
   const SocialState();
   
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-final class SocialInitial extends SocialState {}
+class SocialInitial extends SocialState {}
+
+class SocialLoading extends SocialState {}
+
+class SocialFailure extends SocialState {
+  final String message;
+  const SocialFailure(this.message);
+  @override
+  List<Object?> get props => [message];
+}
+
+// --- Data Loaded States ---
+
+class SocialFeedLoaded extends SocialState {
+  final List<SocialTask> tasks;
+  const SocialFeedLoaded(this.tasks);
+  @override
+  List<Object?> get props => [tasks];
+}
+
+class TaskCommentsLoaded extends SocialState {
+  final List<SocialComment> comments;
+  const TaskCommentsLoaded(this.comments);
+  @override
+  List<Object?> get props => [comments];
+}
+
+class FriendListLoaded extends SocialState {
+  final List<SocialUser> friends;
+  const FriendListLoaded(this.friends);
+  @override
+  List<Object?> get props => [friends];
+}
+
+class PendingRequestsLoaded extends SocialState {
+  final List<ConnectionRequest> requests;
+  const PendingRequestsLoaded(this.requests);
+  @override
+  List<Object?> get props => [requests];
+}
+
+class UserSearchResultsLoaded extends SocialState {
+  final List<SocialUser> users;
+  const UserSearchResultsLoaded(this.users);
+  @override
+  List<Object?> get props => [users];
+}
+
+class SocialUserProfileLoaded extends SocialState {
+  final UserProfile profile;
+  const SocialUserProfileLoaded(this.profile);
+  @override
+  List<Object?> get props => [profile];
+}
+
+class SharedTaskDetailsLoaded extends SocialState {
+  final SocialTask task;
+  const SharedTaskDetailsLoaded(this.task);
+  @override
+  List<Object?> get props => [task];
+}
+
+// --- Action Success States ---
+// These are useful for triggering SnackBars or Navigation
+
+class TaskSharedSuccess extends SocialState {
+  final SocialTask task;
+  const TaskSharedSuccess(this.task);
+}
+
+class CommentAddedSuccess extends SocialState {
+  final SocialComment comment;
+  const CommentAddedSuccess(this.comment);
+}
+
+class ConnectionRequestSentSuccess extends SocialState {
+  final ConnectionRequest request;
+  const ConnectionRequestSentSuccess(this.request);
+}
+
+/// Generic success for actions that return void (Delete, Like, Remove, Handle Request)
+class SocialActionSuccess extends SocialState {
+  final String message;
+  const SocialActionSuccess(this.message);
+  @override
+  List<Object?> get props => [message];
+}
