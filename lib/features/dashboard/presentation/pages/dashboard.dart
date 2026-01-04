63145a8062b2f41beda74f983b63be4df81a66d9
Firaol Gelana <gelanafiraol@gmail.com>
2026-01-04 23:22:35 +0300

    merge with main

--- lib/features/dashboard/presentation/pages/dashboard.dart
+++ lib/features/dashboard/presentation/pages/dashboard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shell_flow_mobile_app/features/calendar/presentation/bloc/calendar_bloc.dart';
 import 'package:shell_flow_mobile_app/features/calendar/presentation/pages/calendar_page.dart';
+import 'package:shell_flow_mobile_app/features/chat/presentation/pages/chat_page.dart';
 import 'package:shell_flow_mobile_app/features/dashboard/presentation/pages/home_page.dart';
 import 'package:shell_flow_mobile_app/features/dashboard/presentation/widgets/nav_bar_widget.dart';
 import 'package:shell_flow_mobile_app/features/social/presentation/pages/social_page.dart';
@@ -19,7 +20,7 @@ class _DashboardState extends State<Dashboard> {
   final List<Widget> _pages = [
     const Center(child: HomePage()),
     const Center(child: SocialPage()),
-    const Center(child: Text('Chat Pagee')),
+    const Center(child: ChatPage()),
     BlocProvider(
       create: (context) => sl<CalendarBloc>()..add(LoadAllTasksEvent()),
       child: const CalendarPage(),
